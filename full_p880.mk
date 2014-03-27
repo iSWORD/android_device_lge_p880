b47da15fc1e849a610bda6473d46631515997baf
dcos <darko.cosic@gmail.com>
2014-03-27 16:56:49 +0100

    Fix vendor
    Change-Id: Ibf49b3f359c723266bf11be00d825da000ea6948

--- full_p880.mk
+++ full_p880.mk
@@ -11,8 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/lge/p880/p880.mk)
+
+$(call inherit-product-if-exists, vendor/lge/p880/p880-vendor.mk)
 
